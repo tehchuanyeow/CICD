cc83bdc711216f56df515da4a3da3018eddd0cac
Age <STUDENT@DESKTOP-G5ACJVG>
2018-08-03 13:47:20 +1000

    Removed Files

--- ChuanYeow_TaskB/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
+++
@@ -1,7 +0,0 @@
-namespace ChuanYeow_TaskB.Areas.HelpPage.ModelDescriptions
-{
-    public class CollectionModelDescription : ModelDescription
-    {
-        public ModelDescription ElementDescription { get; set; }
-    }
-}
