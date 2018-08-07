7ba7b47709f8a4540766ac0d55bfee01efa8324e
tehchuanyeow <tehchuanyeow@yahoo.com>
2018-08-07 15:16:25 +1000

    Failing Unit Test
    Unit Test should Fail because of Assert.

---
+++ ReleasePipeDemo.Tests/Controllers/ExamplesControllerTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ReleasePipeDemo.Controllers;
+
+namespace ReleasePipeDemo.Tests.Controllers
+{
+    [TestClass]
+    public class ExamplesControllerTests
+    {
+        [TestMethod]
+        public void Get()
+        {
+            // Arrange
+            ExamplesController controller = new ExamplesController();
+
+            // Act
+            IEnumerable<string> result = controller.Get();
+
+            // Assert
+            Assert.IsNotNull(result);
+            Assert.AreEqual(2, result.Count());
+            Assert.AreEqual("wrong", result.ElementAt(0));
+            Assert.AreEqual("value2", result.ElementAt(1));
+        }
+    }
+}
